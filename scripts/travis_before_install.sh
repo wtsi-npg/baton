2f27ea20f335bb38acd77f414f981fbee1c395bc
Keith James <kdj@sanger.ac.uk>
2016-11-07 12:45:55 +0000

    Use disposable-irods

--- scripts/travis_before_install.sh
+++ scripts/travis_before_install.sh
@@ -2,36 +2,4 @@
 
 set -e -x
 
-IRODS_VERSION=${IRODS_VERSION:=3.3.1}
-
-before_install_common() {
-    sudo apt-get update -qq
-}
-
-before_install_3_3_1() {
-    return
-}
-
-before_install_4_1_x() {
-    wget ${RENCI_URL}/pub/irods/releases/${IRODS_VERSION}/${PLATFORM}/irods-icat-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-    wget ${RENCI_URL}/pub/irods/releases/${IRODS_VERSION}/${PLATFORM}/irods-database-plugin-postgres-${PG_PLUGIN_VERSION}-${PLATFORM}-${ARCH}.deb
-    wget ${RENCI_URL}/pub/irods/releases/${IRODS_VERSION}/${PLATFORM}/irods-runtime-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-    wget ${RENCI_URL}/pub/irods/releases/${IRODS_VERSION}/${PLATFORM}/irods-dev-${IRODS_VERSION}-${PLATFORM}-${ARCH}.deb
-}
-
-case $IRODS_VERSION in
-
-    3.3.1)
-        before_install_common
-        before_install_3_3_1
-        ;;
-
-    4.1.9)
-        before_install_common
-        before_install_4_1_x
-        ;;
-
-    *)
-        echo Unknown iRODS version $IRODS_VERSION
-        exit 1
-esac
+sudo apt-get update -qq
--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -2,35 +2,15 @@
 
 set -e -x
 
-IRODS_VERSION=${IRODS_VERSION:=3.3.1}
+IRODS_HOME=
+baton_irods_conf="--with-test-resource=testResc --with-irods"
 
-script_common() {
-    autoreconf -fi
-}
+if [ -n "$IRODS_RIP_DIR" ]
+then
+    export IRODS_HOME="$IRODS_RIP_DIR/iRODS"
+    baton_irods_conf="--with-test-resource=testResc --with-irods=$IRODS_HOME"
+fi
 
-script_3_3_1() {
-    ./configure --with-irods=$IRODS_HOME
-    make distcheck DISTCHECK_CONFIGURE_FLAGS=--with-irods=$IRODS_HOME
-}
-
-script_4_1_x() {
-    ./configure --with-test-resource=testResc --with-irods
-    make distcheck DISTCHECK_CONFIGURE_FLAGS=--with-irods
-}
-
-case $IRODS_VERSION in
-
-    3.3.1)
-        script_common
-        script_3_3_1
-        ;;
-
-    4.1.9)
-        script_common
-        script_4_1_x
-        ;;
-
-    *)
-        echo Unknown iRODS version $IRODS_VERSION
-        exit 1
-esac
+autoreconf -fi
+./configure ${baton_irods_conf}
+make distcheck DISTCHECK_CONFIGURE_FLAGS="${baton_irods_conf}"
