5e35550802eeb84659627e98c66d2b0007f8dcb8
mksanger <mk35@sanger.ac.uk>
2022-06-21 17:42:53 +0100

    Remove unneccessary iput for new test

--- tests/check_baton.c
+++ tests/check_baton.c
@@ -37,9 +37,9 @@ int exit_flag;
 static int MAX_COMMAND_LEN = 1024;
 static int MAX_PATH_LEN    = 4096;
 
-static char *TEST_COLL          = "baton-basic-test";
-static char *TEST_DATA_PATH     = "data";
-static char *TEST_METADATA_PATH = "metadata/meta1.imeta";
+static char *TEST_COLL           = "baton-basic-test";
+static char *TEST_DATA_PATH      = "data";
+static char *TEST_METADATA_PATH  = "metadata/meta1.imeta";
 static char *SQL_PATH            = "sql/specific_queries.sql";
 
 static char *SETUP_SCRIPT        = "scripts/setup_irods.sh";
@@ -48,7 +48,7 @@ static char *SQL_SETUP_SCRIPT    = "scripts/setup_sql.sh";
 static char *TEARDOWN_SCRIPT     = "scripts/teardown_irods.sh";
 static char *SQL_TEARDOWN_SCRIPT = "scripts/teardown_sql.sh";
 
-static char *BAD_REPLICA_SCRIPT = "scripts/make_bad_replica.sh";
+static char *BAD_REPLICA_SCRIPT  = "scripts/make_bad_replica.sh";
 
 static void set_current_rods_root(char *in, char *out) {
     rodsEnv rodsEnv;
@@ -2241,14 +2241,10 @@ START_TEST(test_checksum_ignore_stale) {
     rodsEnv env;
     rcComm_t *conn = rods_login(&env);
 
-    char file_path[MAX_PATH_LEN];
-    snprintf(file_path, MAX_PATH_LEN, "%s/%s/lorem_10k.txt",
-             TEST_ROOT, TEST_DATA_PATH);
-
     char rods_root[MAX_PATH_LEN];
     set_current_rods_root(TEST_COLL, rods_root);
 
-    char obj_name[38] = "test_checksum_obj_with_bad_replica.txt";
+    char obj_name[38] = "lorem_10k.txt";
     char obj_path[MAX_PATH_LEN];
     snprintf(obj_path, MAX_PATH_LEN, "%s/%s",
              rods_root, obj_name);
@@ -2259,18 +2255,6 @@ START_TEST(test_checksum_ignore_stale) {
                       flags, &resolve_error);
     ck_assert_int_eq(resolve_error.code, 0);
 
-    baton_error_t put_error;
-    int put_status = put_data_obj(conn, file_path, &rods_obj_path,
-                                  _RESOURCE, NULL, flags, &put_error);
-    ck_assert_int_eq(put_error.code, 0);
-    ck_assert_int_eq(put_status, 0);
-
-    rodsPath_t result_obj_path;
-    baton_error_t result_error;
-    resolve_rods_path(conn, &env, &result_obj_path, obj_path,
-                      flags, &result_error);
-    ck_assert_int_eq(result_error.code, 0);
-
     char command[MAX_COMMAND_LEN];
     // change checksum of replica 1 without marking stale
     snprintf(command, MAX_COMMAND_LEN, "%s/%s -c %s -d %s -a", TEST_ROOT,
@@ -2280,7 +2264,7 @@ START_TEST(test_checksum_ignore_stale) {
     if (ret != 0) raise(SIGTERM);
 
     baton_error_t wrong_checksum_error;
-    json_t *result = list_path(conn, &result_obj_path, PRINT_CHECKSUM,
+    json_t *result = list_path(conn, &rods_obj_path, PRINT_CHECKSUM,
                                &wrong_checksum_error);
     ck_assert_int_ne(wrong_checksum_error.code, 0);
 
@@ -2292,7 +2276,7 @@ START_TEST(test_checksum_ignore_stale) {
     if (ret != 0) raise(SIGTERM);
 
     baton_error_t stale_replica_error;
-    result = list_path(conn, &result_obj_path, PRINT_CHECKSUM,
+    result = list_path(conn, &rods_obj_path, PRINT_CHECKSUM,
                        &stale_replica_error);
     ck_assert_int_eq(stale_replica_error.code, 0);
 
